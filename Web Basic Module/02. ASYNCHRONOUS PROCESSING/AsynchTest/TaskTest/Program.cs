51d485eeb3765cf2a9f08c2e4ba382c30cd53dc6
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-03-09 15:37:10 +0200

    Web Basic Module

---
+++ Web Basic Module/02. ASYNCHRONOUS PROCESSING/AsynchTest/TaskTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Web Basic Module/02. ASYNCHRONOUS PROCESSING/AsynchTest/Test/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal money = 0;
+            var objLock = new object();
+            var thread1 = new Thread(() =>
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    lock (objLock)
+                    {
+                        money++;
+                    }
+                }
+            });
+            thread1.Start();
+
+            var thread2 = new Thread(() =>
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    lock (objLock)
+                    {
+                        money++;
+                    }
+                    
+                }
+            });
+
+            thread2.Start();
+
+            var thread3 = new Thread(() =>
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    lock (objLock)
+                    {
+                        money++;
+                    }
+                }
+            });
+
+            thread3.Start();
+
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+            Console.WriteLine(money);
+        }
+
+        private static void MyThreadMainMethod()
+        {
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine(CountPrimeNumber(1, 10000000));
+            Console.WriteLine(sw.Elapsed);
+        }
+
+        private static int CountPrimeNumber(int from, int to)
+        {
+            int count = 0;
+            for (int i = from; i <= to; i++)
+            {
+                bool isPrime = true;
+                for (int div = 2; div < Math.Sqrt(i); div++)
+                {
+                    if (i % div == 0)
+                    {
+                        isPrime = false;
+                    }
+                }
+                if (isPrime)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}

76aa9bec51ee98df249d15ab65af2861f7b2339a
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-03-09 14:45:55 +0200

    Web Basic Module

--- Web Basic Module/01.WEB SERVER HTTP PROTOCOL/HttpRequester/StartUp.cs
+++ Web Basic Module/01.WEB SERVER HTTP PROTOCOL/HttpRequester/StartUp.cs
@@ -11,64 +11,40 @@ namespace HttpRequester
     class StartUp
     {
 
-        static Dictionary<string, int> SessionStore = new Dictionary<string, int>();
         static async Task Main(string[] args)
         {
-            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 1234);
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 80);
             tcpListener.Start();
             while (true)
             {
                 TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-                Task.Run(() => ProcessClientAsync(tcpClient));
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                const string NewLine = "\r\n";
+                using NetworkStream networkStream = tcpClient.GetStream();
+                byte[] requestBytes = new byte[1000000]; // TODO: Use buffer
+                int bytesRead = await networkStream.ReadAsync(requestBytes, 0, requestBytes.Length);
+                string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
+
+                string responseText = @"<form action= '/Action/Login' method= 'post'>
+<input type= date name = 'date' />
+<input type= text name = 'text' />
+<input type= password name = 'password' />
+<input type= submit name = 'Login' />
+</form>";
+                string response = "HTTP/1.0 200 OK" + NewLine +
+                                  "Server: SoftUniServer/1.0" + NewLine +
+                                  "Content-Type: text/html" + NewLine +
+                                  // "Location: https://google.com" + NewLine +
+                                  // "Content-Disposition: attachment; filename=niki.html" + NewLine +
+                                  "Content-Lenght: " + responseText.Length + NewLine +
+                                  NewLine +
+                                  responseText;
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                await networkStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+                Console.WriteLine(request);
+                Console.WriteLine(new string('=', 60));
             }
         }
 
-        private static async Task ProcessClientAsync(TcpClient tcpClient)
-        {
-            const string NewLine = "\r\n";
-            using NetworkStream networkStream = tcpClient.GetStream();
-            byte[] requestBytes = new byte[1000000]; // TODO: Use buffer
-            int bytesRead = await networkStream.ReadAsync(requestBytes, 0, requestBytes.Length);
-            string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
-
-            var sid = Regex.Match(request, @"sid=[^\n]*\n").Value?.Replace("sid=", string.Empty).Trim();
-            Console.WriteLine(sid);
-            var newSid = Guid.NewGuid().ToString();
-            var count = 0;
-            if (SessionStore.ContainsKey(sid))
-            {
-                SessionStore[sid]++;
-                count = SessionStore[sid];
-            }
-            else
-            {
-                sid = null;
-                SessionStore[newSid] = 1;
-                count = 1;
-            }
-
-
-            string responseText = "<h1>" + "Petroslav Ivov Gochev" + "</h1>" + "<h1>" + "Rosen Stanimirov Bobchev"+ "</h1>";
-            string response = "HTTP/1.0 301 OK" + NewLine +
-                              "Server: SoftUniServer/1.0" + NewLine +
-                              "Content-Type: text/html" + NewLine +
-                              "Set-Cookie: user=Petroslav; Max-Age: 2500; HttpOnly;" + NewLine +
-                              (string.IsNullOrWhiteSpace(sid) ?
-                                ("Set-Cookie: sid=" + newSid + NewLine)
-                                : string.Empty) +
-                               //"Location: https://google.com" + NewLine +
-                              // "Content-Disposition: attachment; filename=niki.html" + NewLine +
-                              "Content-Lenght: " + responseText.Length + NewLine +
-                              NewLine +
-                              responseText;
-            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-            await networkStream.WriteAsync(responseBytes, 0, responseBytes.Length);
-            Console.WriteLine(request);
-            Console.WriteLine(response.Length);
-            Console.WriteLine(new string('=', 60));
-        }
     }
 }
 
---
+++ Web Basic Module/02. ASYNCHRONOUS PROCESSING/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DownloadImage(this.Image1, "https://dogtime.com/assets/uploads/gallery/french-bulldog-dog-breed-pictures/1-puppy.jpg");
+            DownloadImage(this.Image2, "https://vetstreet.brightspotcdn.com/dims4/default/5298792/2147483647/crop/0x0%2B0%2B0/resize/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F3d%2F174bd0a0d511e0a2380050568d634f%2Ffile%2FFrench-Bulldog-5-645mk062111.jpg");
+            DownloadImage(this.Image3, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFDgzaB4pqngz_t2rpxdM550RcSxSSCUDOSQ&usqp=CAU");
+        }
+        
+        private void DownloadImage(Image image, string url)
+        {
+            var client = new HttpClient();
+            var request = client.GetAsync(url).GetAwaiter().GetResult();
+            var dataBytes = request.Content.ReadAsByteArrayAsync().GetAwaiter().GetResult();
+            image.Source = this.LoadImage(dataBytes);
+        }
+        private  BitmapImage LoadImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
+        }
+    }
+}

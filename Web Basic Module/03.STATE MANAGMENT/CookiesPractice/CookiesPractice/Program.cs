165259c15bc5f1fde808ddf5447343b83f99f5e6
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-03-10 14:31:15 +0200

    Web Basic Module

--- Web Basic Module/03.STATE MANAGMENT/CookiesPractice/CookiesPractice/Program.cs
+++ Web Basic Module/03.STATE MANAGMENT/CookiesPractice/CookiesPractice/Program.cs
@@ -1,45 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CookiesPractice
 {
     class Program
     {
+        private static Dictionary<string, int> sessionStore = new Dictionary<string, int>();
+        const string NewLine = "\r\n";
         static async Task Main(string[] args)
         {
-            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 80);
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 3333);
             tcpListener.Start();
             while (true)
             {
                 TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-                Task.Run(() => ProcessClient(tcpClient));
+                await Task.Run(() => ProcessClient(tcpClient));
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
             }
+
+
+
         }
 
         private static async Task ProcessClient(TcpClient tcpClient)
         {
-            const string NewLine = "\r\n";
+            
             NetworkStream networkStream = tcpClient.GetStream();
+
             byte[] requestBytes = new byte[1000000]; // TODO: Use buffer
             int bytesRead = await networkStream.ReadAsync(requestBytes, 0, requestBytes.Length);
             string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
+            var regex = @"sid=(?<value>.+)";
+            var sid = Regex.Match(request, regex);
 
-            string responseText = @"<form action= '/Action/Login' method= 'post'>
-<input type= date name = 'date' />
-<input type= text name = 'text' />
-<input type= password name = 'password' />
-<input type= submit name = 'Login' />
-</form>";
+
+            string responseText = @"<h1>" + sid.Groups["value"].ToString().Split(";")[0] + "</h1>";
             string response = "HTTP/1.0 200 OK" + NewLine +
                               "Server: SoftUniServer/1.0" + NewLine +
                               "Content-Type: text/html" + NewLine +
                               // "Location: https://google.com" + NewLine +
-                              // "Content-Disposition: attachment; filename=niki.html" + NewLine +
+                              // "Content-Disposition: attachment; filename=petroslav.html" + NewLine +
+                              //"Set-Cookie: language=bg; user=petroslav; path=/; secure; SameSite=Strict" + NewLine +
+                              (string.IsNullOrWhiteSpace(sid.Groups["value"].ToString().Split(";")[0]) ? "Set-Cookie: sid=" + Guid.NewGuid().ToString()+ NewLine : string.Empty) + 
                               "Content-Lenght: " + responseText.Length + NewLine +
                               NewLine +
                               responseText;
@@ -50,4 +60,5 @@ namespace CookiesPractice
             //return networkStream;
         }
     }
+
 }

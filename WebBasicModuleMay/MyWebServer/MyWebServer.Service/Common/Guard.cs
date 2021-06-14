de35fa9545efe5aaea23b1b975df166132a152a2
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-06-14 12:46:51 +0300

    Create guard static class against null text

---
+++ WebBasicModuleMay/MyWebServer/MyWebServer.Service/Common/Guard.cs
@@ -0,0 +1,17 @@
+﻿namespace MyWebServer.Service.Common
+{
+    using System;
+
+    public static class Guard
+    {
+        public static void AgainstNull(object value, string name = null)
+        {
+            if (value == null)
+            {
+                name ??= "Value";
+
+                throw new ArgumentException($"{name} cannot be null!");
+            }
+        }
+    }
+}

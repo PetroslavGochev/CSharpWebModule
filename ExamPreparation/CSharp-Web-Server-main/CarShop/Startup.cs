02846cd8412b3a22b541cf06cb4712b0a049b28f
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-06-21 11:46:31 +0300

    CarShop

---
+++ ExamPreparation/CSharp-Web-Server-main/CarShop/Startup.cs
@@ -0,0 +1,31 @@
+﻿namespace CarShop
+{
+    using System.Threading.Tasks;
+
+    using CarShop.Data;
+    using CarShop.Services;
+    using Microsoft.EntityFrameworkCore;
+    using MyWebServer;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+
+    public class Startup
+    {
+        public static async Task Main()
+            => await HttpServer
+                .WithRoutes(routes => routes
+                    .MapStaticFiles()
+                    .MapControllers())
+                .WithServices(services => services
+                    .Add<IViewEngine, CompilationViewEngine>()
+                    .Add<IValidator, Validator>()
+                    .Add<IPasswordService, PasswordService>()
+                    .Add<IUserService, UserService>()
+                    .Add<ICarService, CarService>()
+                    .Add<IIssueService, IssueService>()
+                    .Add<CarShopDbContext>())
+                .WithConfiguration<CarShopDbContext>(context => context
+                    .Database.Migrate())
+                .Start();
+    }
+}

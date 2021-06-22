ce9eae1d2dda1395887a6a2ee0772cbc65ed8db4
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-06-22 10:44:09 +0300

    Views

---
+++ ExamPreparation/CSharp-Web-Server-main/SulsProblemDescription/Startup.cs
@@ -0,0 +1,32 @@
+﻿namespace SulsProblemDescription
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.EntityFrameworkCore;
+    using MyWebServer;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+    using SulsApp.Data;
+    using SulsProblemDescription.Services;
+
+    public class Startup
+    {
+        static async Task Main(string[] args)      
+           => await HttpServer
+               .WithRoutes(routes => routes
+                   .MapStaticFiles()
+                   .MapControllers())
+               .WithServices(services => services
+                   .Add<IViewEngine, CompilationViewEngine>()
+                   .Add<IPasswordService, PasswordService>()
+                   .Add<IUserService, UserService>()
+                   .Add<IProblemService, ProblemService>()
+                   .Add<ISubmissionService, SubmissionService>()
+                   .Add<IValidatorService, ValidatorService>()
+                   .Add<ApplicationDbContext>())
+               .WithConfiguration<ApplicationDbContext>(context => context
+                   .Database.Migrate())
+               .Start();
+        
+    }
+}

71be78c814479c66b34963fc67f08087b02d16d4
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-06-23 10:08:33 +0300

    Views

---
+++ ExamPreparation/CSharp-Web-Server-main/SharedTrip/Startup.cs
@@ -0,0 +1,30 @@
+﻿namespace SharedTrip
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.EntityFrameworkCore;
+    using MyWebServer;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+    using SharedTrip.Data;
+    using SharedTrip.Services;
+
+    public class Startup
+    {
+        static async Task Main(string[] args)
+           => await HttpServer
+               .WithRoutes(routes => routes
+                   .MapStaticFiles()
+                   .MapControllers())
+               .WithServices(services => services
+                   .Add<IViewEngine, CompilationViewEngine>()
+                   .Add<IPasswordService, PasswordService>()
+                   .Add<IValidatorService, ValidatorService>()
+                   .Add<IUserService, UserService>()
+                   .Add<ITripService, TripService>()
+                   .Add<ApplicationDbContext>())
+               .WithConfiguration<ApplicationDbContext>(context => context
+                   .Database.Migrate())
+               .Start();
+    }
+}

71e3d6100c5ffe6ae689cae95128834bbc14f343
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-06-23 10:08:15 +0300

    ViewModel

---
+++ ExamPreparation/CSharp-Web-Server-main/SharedTrip/ViewModel/Trip/AllTripViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SharedTrip.ViewModel.Trip
+{
+    using System;
+
+    public class AllTripViewModel
+    {
+        public string Id { get; set; }
+
+        public string StartPoint { get; set; }
+
+        public string EndPoint { get; set; }
+
+        public DateTime DepartureTime { get; set; }
+
+        public int Seats { get; set; }
+    }
+}
---
+++ ExamPreparation/CSharp-Web-Server-main/SharedTrip/ViewModel/Trip/DetailsViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace SharedTrip.ViewModel.Trip
+{
+    using System;
+
+    public class DetailsViewModel
+    {
+        public string Id { get; set; }
+
+        public string StartPoint { get; set; }
+
+        public string EndPoint { get; set; }
+
+        public DateTime DepartureTime { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string Description { get; set; }
+
+        public int Seats { get; set; }
+    }
+}

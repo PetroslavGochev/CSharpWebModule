faa36a8f437c7aad2a8582a309a08a69e6b3f6d5
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-06-24 10:30:35 +0300

    IRunes Views

---
+++ ExamPreparation/CSharp-Web-Server-main/IRunes/Startup.cs
@@ -0,0 +1,32 @@
+﻿namespace IRunes
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.EntityFrameworkCore;
+    using IRunes.Data;
+    using MyWebServer;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+    using IRunes.Services;
+
+    public class Startup
+        {
+            static async Task Main(string[] args)
+               => await HttpServer
+                   .WithRoutes(routes => routes
+                       .MapStaticFiles()
+                       .MapControllers())
+                   .WithServices(services => services
+                       .Add<IViewEngine, CompilationViewEngine>()
+                       .Add<IPasswordService, PasswordService>()
+                       .Add<IValidatorService, ValidatorService>()
+                       .Add<IAlbumService, AlbumService>()
+                       .Add<ITrackService, TrackService>()
+                       .Add<IUserService, UserService>()
+                       .Add<ApplicationDbContext>())
+                   .WithConfiguration<ApplicationDbContext>(context => context
+                       .Database.Migrate())
+                   .Start();
+        }
+    
+}

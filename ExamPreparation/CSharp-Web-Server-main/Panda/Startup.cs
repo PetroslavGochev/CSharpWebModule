4a83eb5ee7cebad01875b55911fe447783eb71f4
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-06-25 11:31:14 +0300

    Panda Views

---
+++ ExamPreparation/CSharp-Web-Server-main/Panda/Startup.cs
@@ -0,0 +1,31 @@
+﻿namespace Panda
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using MyWebServer;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+    using Panda.Data;
+    using Panda.Services;
+
+    public class Startup
+    {
+        static async Task Main(string[] args)
+               => await HttpServer
+                   .WithRoutes(routes => routes
+                       .MapStaticFiles()
+                       .MapControllers())
+                   .WithServices(services => services
+                       .Add<IViewEngine, CompilationViewEngine>()
+                       .Add<IPasswordService, PasswordService>()
+                       .Add<IUserService, UserService>()
+                       .Add<IPackageService, PackageService>()
+                       .Add<IRecipientService, RecipientService>()
+                       .Add<IValidatorService, ValidatorService>()
+                       .Add<ApplicationDbContext>())
+                   .WithConfiguration<ApplicationDbContext>(context => context
+                       .Database.Migrate())
+                   .Start();
+    }
+}

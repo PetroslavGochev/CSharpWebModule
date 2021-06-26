5fb4dab80a37e8c6652fa594ffb67c1897c282ad
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-06-26 09:21:56 +0300

    BattleCards ViewModels

---
+++ ExamPreparation/CSharp-Web-Server-main/BattleCards/ViewModels/Cards/CardViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace BattleCards.ViewModels.Cards
+{
+    public class CardViewModel
+    {
+        public string Id { get; set; }
+
+        public string Keyword { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int Attack { get; set; }
+
+        public int Health { get; set; }
+
+        public string Description { get; set; }
+    }
+}

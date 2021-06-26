b4262ed04c5a7ae3f6bc121fe1cb7380eeda95f0
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-06-26 09:22:27 +0300

    BattleCards views and global constants

---
+++ ExamPreparation/CSharp-Web-Server-main/BattleCards/Common/GlobalConstants.cs
@@ -0,0 +1,34 @@
+﻿namespace BattleCards.Common
+{
+    public class GlobalConstants
+    {
+        // VALIDATION 
+
+        public const string RequiredField = "The field {0} is required";
+
+        public const string MaxMinLength = "{0} is not valid. {0} should be between {1} and {2} length.";
+
+        public const string NotNegative = "{0} cannot be negative number";
+
+        public const string EqualPassword = "Password and confirm password don`t match";
+
+        public const string Exist = "This {0} already exist";
+
+
+        // Users
+
+        public const int MinLengthUsername = 5;
+
+        public const int MaxLengthUsername = 20;
+
+        // Cards
+
+        public const int MinLengthName = 5;
+
+        public const int MaxLengthName = 15;
+
+        public const int DescriptionMaxLength = 200;
+
+        public const int Minimum = 0;
+    }
+}
---
+++ ExamPreparation/CSharp-Web-Server-main/BattleCards/Startup.cs
@@ -0,0 +1,31 @@
+﻿namespace BattleCards
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using BattleCards.Data;
+    using BattleCards.Services;
+    using Microsoft.EntityFrameworkCore;
+    using MyWebServer;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+
+    public class Startup
+    {
+        public static async Task Main()
+           => await HttpServer
+               .WithRoutes(routes => routes
+                   .MapStaticFiles()
+                   .MapControllers())
+               .WithServices(services => services
+                   .Add<IViewEngine, CompilationViewEngine>()
+                   .Add<IValidatorService, ValidatorService>()
+                   .Add<IPasswordService, PasswordService>()
+                   .Add<IUserService, UserService>()
+                   .Add<ICardService, CardService>()
+                   .Add<ApplicationDbContext>())
+               .WithConfiguration<ApplicationDbContext>(context => context
+                   .Database.Migrate())
+               .Start();
+    }
+}
